2a37271038109790ff29d38ce0512ee7d1245f2d
bondparkerbond <parkerbond@gmail.com>
2016-01-04 10:51:41 -0700

    materialize card to prepare for react stuff

--- app/assets/javascripts/components/List.js.jsx
+++ app/assets/javascripts/components/List.js.jsx
@@ -14,6 +14,11 @@ var List = React.createClass({
   render: function() {
     return( <div>
               <a className='waves-effect waves-light btn' onClick={this.showAddForm}>Add Item</a>
+              <div className='card blue-grey darken-1'>
+                <div className='card-content white-text'>
+                  <span className='card-title'>To Do</span>
+                </div>
+              </div>
             </div> );
   }
 });
