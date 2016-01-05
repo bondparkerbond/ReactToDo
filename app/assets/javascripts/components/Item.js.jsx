51fcbb5c0aa426db8a463cb7034ca0b09a85b476
bondparkerbond <parkerbond@gmail.com>
2016-01-05 12:01:55 -0700

    can edit items now and only 1 render function

--- app/assets/javascripts/components/Item.js.jsx
+++ app/assets/javascripts/components/Item.js.jsx
@@ -19,13 +19,13 @@ var Item = React.createClass({
     });
   },
 
-  render: function() {
+  item: function() {
     var id = "item-" + this.props.id;
     var checked = this.props.complete ? 'checked' : '';
     var itemClass = 'col s10 ' + checked;
     return( <li>
               <div className='row'>
-                <div className={itemClass}>
+                <div onClick={this.toggleEdit} className={itemClass}>
                   {this.props.name}
                 </div>
                 <div onClick={this.checkItem} className='col s2'>
@@ -36,11 +36,39 @@ var Item = React.createClass({
             </li>);
   },
 
+  edit: function() {
+    return( <li>
+              <div className='row'>
+                <div className='col s10'>
+                  <form onSubmit={this.updateItem}>
+                    <input autoFocus={true} type='text' defaultValue={this.props.name} ref='itemName' />
+                  </form>
+                </div>
+                <div className='col s2'>
+                  <a onClick={this.toggleEdit}>Cancel</a>
+                </div>
+              </div>
+            </li>);
+  },
+
+  updateItem: function() {
+    var name = ReactDOM.findDOMNode(this.refs.itemName).value;
+    var self = this;
+    $.ajax({
+      url: '/items/' + this.props.id,
+      type: 'PUT',
+      data: { item: { name: name }},
+      success: function() {
+        self.props.refreshList();
+      }
+    });
+  },
+
   render: function() {
-    if (this.state.edit) {
+    if (this.state.edit) 
       return this.edit();
-    } else {
-        return this.item();
-    }
+    else 
+      return this.item();
+    
   }
 });
